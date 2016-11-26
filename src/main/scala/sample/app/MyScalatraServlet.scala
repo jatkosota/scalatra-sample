2e24b14cd180abb146c78348e88ccd74c3591669
jatkosota <jatkosota.mika@gmail.com>
2016-11-26 19:07:00 +0900

    init commit

---
+++ src/main/scala/sample/app/MyScalatraServlet.scala
@@ -0,0 +1,16 @@
+package sample.app
+
+import org.scalatra._
+
+class MyScalatraServlet extends ScalatrasampleStack {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!</h1>
+        Say <a href="hello-scalate">hello to Scalate</a>.
+      </body>
+    </html>
+  }
+
+}
